d8074858b72419655746eca0eaa3f1ea3c0d5b4b
manoj-kumar-vst-au4 <mauryamanoj513@gmail.com>
2020-03-10 15:30:20 +0530

    compare the triplets solved

---
+++ compareTheTriplets.js
@@ -0,0 +1,15 @@
+function compareTriplets(a, b) {
+    let alice = 0;
+    let bob = 0;
+    for(let i = 0; i < 3; i++){
+        if(a[i] > b[i]){
+            ++alice;
+        }
+        else if(a[i] < b[i]){
+            ++bob;
+        }
+    }
+    return [alice, bob];
+}
+
+console.log(compareTriplets([5, 6, 7], [3, 6, 10]));
